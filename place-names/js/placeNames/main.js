a7d89c8045292eb07d9e739a994233edecc15d25
Daniel Hero <danielhero@outlook.com>
2020-05-26 13:57:01 -0500

    finished

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -1,6 +1,21 @@
 function main() {
-  // Put your code here
+  const names = ["Nashville", "Hong Kong", "The back yard", "Earth",
+    "London", "The mall", "Ryman Auditorium", "The Great Wall of China"]
 
+
+  console.log("All Place Names")
+
+  for (name of names) {
+    console.log(name)
+  }
+
+  const theNames = names.filter(name => name.startsWith("The"))
+
+  console.log('"The" Place Names')
+
+  for (name of theNames) {
+    console.log(name)
+  }
 }
 
 main();
